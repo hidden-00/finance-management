803e0317925a402342987f815c4a53b186339f4c
hidden-00 <maixuanytp3@gmail.com>
2024-02-17 11:46:09 +0700

    UPdate validate and sanitize User

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const UserController = require('../controllers/user.controller')
 const auth = require('../middlewares/auth.middleware')
+const validateUser = require('../validation/user');
 const router = express.Router()
 
-router.post('/', UserController.createOne)
+router.post('/', validateUser.sanitizeRegister, validateUser.validateRegisterInput, UserController.createOne)
 router.get('/', auth.authentication, UserController.getAll)
-router.post('/login', auth.getClientInfo, UserController.login);
+router.post('/login', validateUser.sanitizeLogin, validateUser.validateLoginInput, auth.getClientInfo, UserController.login);
 router.post('/logout', auth.authentication, UserController.logout);
 router.get('/info', auth.authentication, UserController.getMe);
 
---
+++ backend/validation/user.js
@@ -0,0 +1,134 @@
+const httpStatus = require('http-status');
+const sanitizeHelper = require('../helpers/sanitize');
+const sendResponse = require('../helpers/sendResponse');
+const validateHelper = require('../helpers/validate');
+const isEmpty = require('./isEmpty');
+
+const validations = {};
+
+validations.sanitizeRegister = (req, res, next) => {
+    const sanitizeArray = [
+        {
+            field: 'name',
+            sanitize: {
+                trim: true
+            }
+        },
+        {
+            field: 'email',
+            sanitize: {
+                trim: true,
+            },
+        }
+    ];
+    sanitizeHelper.sanitize(req, sanitizeArray);
+    next();
+}
+
+validations.sanitizeLogin = (req, res, next) => {
+    const sanitizeArray = [
+        {
+            field: 'email',
+            sanitize: {
+                trim: true,
+            },
+        },
+    ];
+    sanitizeHelper.sanitize(req, sanitizeArray);
+    next();
+};
+
+validations.validateLoginInput = (req, res, next) => {
+    const data = req.body;
+    const validateArray = [
+        {
+            field: 'email',
+            validate: [
+                {
+                    condition: 'IsEmpty',
+                    msg: 'This field is required',
+                },
+                {
+                    condition: 'IsEmail',
+                    msg: 'Please enter Valid Email',
+                },
+            ],
+        },
+        {
+            field: 'password',
+            validate: [
+                {
+                    condition: 'IsEmpty',
+                    msg: 'This field is required',
+                },
+                {
+                    condition: 'IsLength',
+                    msg: 'password must be at least 6 characters, max limit 30 characters',
+                    option: { min: 6, max: 30 },
+                },
+            ],
+        },
+    ];
+    const errors = validateHelper.validation(data, validateArray);
+    if (!isEmpty(errors)) {
+        return sendResponse(res, httpStatus.BAD_REQUEST, false, null, errors, 'invalid input', null);
+    } else {
+        next();
+    }
+};
+
+validations.validateRegisterInput = (req, res, next) => {
+    const data = req.body;
+    const validateArray = [
+        {
+            field: 'name',
+            validate: [
+                {
+                    condition: 'IsEmpty',
+                    msg: 'This field is required',
+                },
+                {
+                    condition: 'IsLength',
+                    msg: 'This field should be between 2 to 100',
+                    option: { min: 2, max: 30 },
+                },
+            ],
+        },
+        {
+            field: 'email',
+            validate: [
+                {
+                    condition: 'IsEmpty',
+                    msg: 'This field is required',
+                },
+                {
+                    condition: 'IsEmail',
+                    msg: 'Please enter Valid Email',
+                },
+            ],
+        },
+        {
+            field: 'password',
+            validate: [
+                {
+                    condition: 'IsEmpty',
+                    msg: 'This field is required',
+                },
+                {
+                    condition: 'IsLength',
+                    msg: 'password must be at least 6 characters, max limit 30 characters',
+                    option: { min: 6, max: 30 },
+                },
+            ],
+        },
+    ];
+    const errors = validateHelper.validation(data, validateArray);
+    if (!isEmpty(errors)) {
+        
+        return sendResponse(res, httpStatus.BAD_REQUEST, false, null, errors, 'invalid input', null);
+    } else {
+        next();
+    }
+};
+
+module.exports = validations;
