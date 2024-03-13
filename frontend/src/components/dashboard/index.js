7391ebedf9ba05a45627c6d151e1e93c54f1d030
hidden-00 <maixuanytp3@gmail.com>
2024-03-13 12:33:43 +0700

    readme

--- frontend/src/components/dashboard/index.js
+++ frontend/src/components/dashboard/index.js
@@ -8,6 +8,20 @@ const Dashboard = () => {
     <div className="container">
       <div>
         <h1>Welcome! {auth.user?.name}</h1>
+        <h2>Finance Management, Mike</h2>
+        <text>The website provides tools for users to manage personal or group finances.</text>
+        <h2>
+        Stack Technology (MERN)
+        </h2>
+        <li>Backend: NodeJS - ExpressJS</li>
+        <li>Frontend: ReactJS</li>
+        <li>Database: MongoDB</li>
+        <h2>Features</h2>
+        <li>Manage personal finance</li>
+        <li>Group financial management</li>
+        <br/>
+        <text>The source code is designed to serve both individuals and groups. You can create your own group and invite other members to your financial group, sharing personal financial income and expenses together in groups.</text>
+        <br></br>
         <Button
           onClick={()=> auth.logOut()}
           variant="contained"
